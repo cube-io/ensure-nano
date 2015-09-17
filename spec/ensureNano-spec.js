74f23611b286252356c0ea58d26c604c53e3b058
Kenn Leth Hansen <klh@justfone.dk>
2015-09-17 07:08:49 +0200

    Describe some tests

---
+++ spec/ensureNano-spec.js
@@ -0,0 +1,9 @@
+describe("ensureNano", function() {
+    it("can take a Nano-object as an argument", function(done) {});
+
+    describe("on first request", function() {
+        it("checks if the database requested exists", function(done) {});
+
+        it("creates the database if it does not exists", function(done) {});
+    });
+});
