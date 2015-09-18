a9f57569d05100feb5e8470142f8cb6872174915
Kenn Leth Hansen <klh@justfone.dk>
2015-09-18 08:38:29 +0200

    Test functionality

--- spec/ensureNano-spec.js
+++ spec/ensureNano-spec.js
@@ -42,8 +42,69 @@ describe("ensureNano", function() {
     });
 
     describe("on first request", function() {
-        it("checks if the database requested exists", function(done) {});
+        it("it creates the database and insert the document", function(done) {
+            var db = ensureNano(nano, "test-database");
 
-        it("creates the database if it does not exists", function(done) {});
+            db.insert({"test": true}, "test", function(error, body) {
+                expect(Object.keys(couchDb.databases)[0]).toBe("test-database");
+                expect(error).toBeNull();
+                expect(body).toBeDefined();
+                done();
+            });
+        });
+
+        it("It inserts the document into the database", function(done) {
+            couchDb.addDB("test-database");
+            var db = ensureNano(nano, "test-database");
+
+            db.insert({"test": true}, "test", function(error, body) {
+                expect(error).toBeNull();
+                expect(body).toBeDefined();
+                done();
+            });
+        });
+    });
+
+    describe("multiple databases", function() {
+        it("can handle more than one database or nano object", function(done) {
+            var nano1 = nano;
+            var nano2 = nanoLib("http://localhost:5984");
+
+            var db1 = ensureNano(nano1, "db1");
+            var db2 = ensureNano(nano2, "db2");
+
+            expect(db1.config.url).toBe(connectionString);
+            expect(db1.config.db).toBe("db1");
+
+            expect(db2.config.url).toBe("http://localhost:5984");
+            expect(db2.config.db).toBe("db2");
+
+            done();
+        });
+
+        it("can insert (and get) documents into/from two different databases", function(done) {
+            var db1 = ensureNano(nano, "db1");
+            var db2 = ensureNano(nano, "db2");
+
+            db1.insert({"test": true}, "test1", function(error, body) {
+                expect(Object.keys(couchDb.databases)[0]).toBe("db1");
+                expect(error).toBeNull();
+                expect(body).toBeDefined();
+                db2.insert({"test": true}, "test2", function(error, body) {
+                    expect(Object.keys(couchDb.databases)[1]).toBe("db2");
+                    expect(error).toBeNull();
+                    expect(body).toBeDefined();
+                    db1.get("test1", function(error, document) {
+                        expect(error).toBeNull();
+                        expect(document).toBeDefined();
+                        db2.get("test2", function(error, document) {
+                            expect(error).toBeNull();
+                            expect(document).toBeDefined();
+                            done();
+                        });
+                    });
+                });
+            });
+        });
     });
 });
