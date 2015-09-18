08f2e7c0669ae863611660ad2b35cd993c8ef519
Kenn Leth Hansen <klh@justfone.dk>
2015-09-18 08:37:54 +0200

    Set up tests

--- spec/ensureNano-spec.js
+++ spec/ensureNano-spec.js
@@ -1,3 +1,24 @@
+var mockCouch = require("mock-couch");
+var nanoLib = require("nano");
+
+var ensureNano = require("../index.js");
+
+var couchDb;
+var connectionString;
+var nano;
+
+beforeEach(function() {
+    var tcpPort = 32125;
+    couchDb = mockCouch.createServer({ keepAlive: false });
+    couchDb.listen(tcpPort);
+    connectionString = "http://localhost:" + tcpPort;
+    nano = nanoLib(connectionString);
+});
+
+afterEach(function() {
+    couchDb.close();
+});
+
 describe("ensureNano", function() {
     it("can take a Nano-object as an argument", function(done) {});
 
